024f67c13364c76faad5ada6dfed37daa1655207
M Ibad Guthwara <43487906+ibadguthwara@users.noreply.github.com>
2019-01-20 23:27:22 +0700

    Initial commit

---
+++ src/pbo3ulang/pkg10116049/latihan1/perintahmenampilkanvaluekelayar/PBO3Ulang10116049Latihan1PerintahMenampilkanValueKeLayar.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3ulang.pkg10116049.latihan1.perintahmenampilkanvaluekelayar;
+
+/**
+ *
+ * @author ibadguthwara
+ * NAMA              : M. Ibad Guthwara
+ * KELAS             : PBO 3 ULANG
+ * NIM               : 10116049
+ * DESKRIPSI PROGRAM : Program ini berisi program untuk menampilkan jenis-jenis tipe data bilangan bulat 
+ */
+ 
+public class PBO3Ulang10116049Latihan1PerintahMenampilkanValueKeLayar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA SAYA" + "\n" + "----------------");
+        System.out.println("NAMA\t\t: M. Ibad Guthwara");
+        System.out.println("ALAMAT\t\t: Jalan Dago Pojok 36");
+        System.out.println("TEMPAT LAHIR\t: Batam");
+        System.out.println("TANGGAL LAHIR\t: 23 Februari 1996");
+        System.out.println("----------------");
+        System.out.println("HOBI\t\t: Futsal dan Badminton");
+        System.out.println("MAKES\t\t: Sate Padang");
+        System.out.println("MIKES\t\t: Jus Alpuket");
+    }
+    
+}
